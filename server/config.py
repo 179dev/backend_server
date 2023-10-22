f39e6619dd315681dba4e02c88638ff4bf50136d
spdz <spidepog@gmail.com>
2023-10-22 21:14:36 +0300

    Moved all environment variables access to config.py

--- server/config.py
+++ server/config.py
@@ -1,8 +1,20 @@
 import os
 
 INNER_PORT = os.getenv("INNER_PORT")
+POSTGRES_DB_NAME = os.getenv("POSTGRES_DB_NAME")
+POSTGRES_USERNAME = os.getenv("POSTGRES_USERNAME")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
-if not INNER_PORT:
+if not (
+    POSTGRES_DB_NAME
+    and POSTGRES_USERNAME
+    and POSTGRES_HOST
+    and POSTGRES_PORT
+    and POSTGRES_PASSWORD
+    and INNER_PORT
+):
     raise ValueError("Incomplete environment variables.")
 
 ALLOWED_ORIGINS = [
--- server/database/db_settings.py
+++ server/database/db_settings.py
@@ -4,14 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from config import *
 
 load_dotenv()
-
-POSTGRES_DB_NAME = os.getenv("POSTGRES_DB_NAME")
-POSTGRES_USERNAME = os.getenv("POSTGRES_USERNAME")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
 if not (
     POSTGRES_DB_NAME
