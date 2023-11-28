bc7699cb0bf2f0f513da879c06b0e245e6efa523
Zhur06 <zhur06@gmail.com>
2023-11-28 19:37:01 +0300

    + Frontend port added to ALLOWED_ORIGINS

--- server/config.py
+++ server/config.py
@@ -3,6 +3,7 @@ import datetime
 
 # Env variables
 INNER_PORT = os.getenv("INNER_PORT")
+FRONTEND_PORT = os.getenv("FRONTEND_PORT")
 POSTGRES_DB_NAME = os.getenv("POSTGRES_DB_NAME")
 POSTGRES_USERNAME = os.getenv("POSTGRES_USERNAME")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
@@ -29,6 +30,7 @@ if not (
     and POSTGRES_PORT
     and POSTGRES_PASSWORD
     and INNER_PORT
+    and FRONTEND_PORT
 ):
     raise ValueError("Incomplete environment variables.")
 
@@ -36,7 +38,9 @@ ALLOWED_ORIGINS = [
     "http://0.0.0.0",
     "http://0.0.0.0:8000",
     f"http://0.0.0.0:{INNER_PORT}",
+    f"http://0.0.0.0:{FRONTEND_PORT}",
     "http://localhost",
     "http://localhost:8000",
     f"http://localhost:{INNER_PORT}",
+    f"http://localhost:{FRONTEND_PORT}",
 ]
