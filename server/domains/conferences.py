7b752eb6c9f00ff1adb58fca8fe52df358edf6f0
spdz <spidepog@gmail.com>
2023-12-08 13:18:40 +0300

    Added DI

--- server/domains/conferences.py
+++ server/domains/conferences.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, Depends
 from fastapi.responses import JSONResponse, Response
 
-from server.conference import main_conferences_pool
+from server.conference.conferences_pool import ConferencesPool
 from server.conference.exceptions import ConferenceNotFound
 from server.conference.member_connections import WebsocketMemberConnection
 from server.conference.types import ConferenceID
@@ -9,25 +9,33 @@ from server.conference.types import ConferenceID
 router = APIRouter()
 
 
-_id = None
+def get_conferences_pool():
+    from server.conference import main_conferences_pool
+
+    return main_conferences_pool
 
 
 @router.post("/conference/")
-async def post():
+async def post(conferences_pool: ConferencesPool = Depends(get_conferences_pool)):
     global _id
-    conference_controller = main_conferences_pool.create_conference()
+    conference_controller = conferences_pool.create_conference()
     _id = conference_controller.conference_id
     return JSONResponse({"conference_id": str(conference_controller.conference_id)})
 
 
 @router.websocket("/ws/conference/{conference_id}")
-async def websocket_endpoint(conference_id: str, websocket: WebSocket):
+async def websocket_endpoint(
+    conference_id: str,
+    websocket: WebSocket,
+    conferences_pool: ConferencesPool = Depends(get_conferences_pool),
+):
     connection = WebsocketMemberConnection(websocket)
     try:
-        conference_controller = main_conferences_pool.get_conference(
+        conference_controller = conferences_pool.get_conference(
             ConferenceID(conference_id)
         )
     except ConferenceNotFound:
-        raise Response("Conference does not exist", status_code=404)
+        await connection.diconnect()
+        return Response("Conference does not exist", status_code=404)
     await connection.connect()
     await conference_controller.run_connection_loop(connection)
